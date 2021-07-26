96a20d5340a3235606f7ca6197cd5e61dc7bbf7f
zhangyu201 <zhangyu201@meituan.com>
2021-07-26 11:30:47 +0800

    add

--- refactoring/src/main/java/com/code/kata/refactoring/ch7/movingFeaturesBetweenObjects/extractClass/step1/Person.java
+++ refactoring/src/main/java/com/code/kata/refactoring/ch7/movingFeaturesBetweenObjects/extractClass/step1/Person.java
@@ -25,10 +25,7 @@ public class Person {
 
 
     private String name;
-    private String officeAreaCode;
-    private String officeNumber;
-    //Create new class
-    private TelephoneNumber officeTelephone = new TelephoneNumber();
+
 
     public String getName() {
         return name;
@@ -38,19 +35,4 @@ public class Person {
         this.name = name;
     }
 
-    public String getOfficeAreaCode() {
-        return officeAreaCode;
-    }
-
-    public void setOfficeAreaCode(String officeAreaCode) {
-        this.officeAreaCode = officeAreaCode;
-    }
-
-    public String getOfficeNumber() {
-        return officeNumber;
-    }
-
-    public void setOfficeNumber(String officeNumber) {
-        this.officeNumber = officeNumber;
-    }
 }
--- refactoring/src/main/java/com/code/kata/refactoring/ch7/movingFeaturesBetweenObjects/extractClass/step1/TelephoneNumber.java
+++
@@ -1,8 +0,0 @@
-package com.code.kata.refactoring.ch7.movingFeaturesBetweenObjects.extractClass.step1;
-
-/**
- * @author zhangyu201
- * @date 2021/7/23
- */
-public class TelephoneNumber {
-}
